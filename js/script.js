35e96dc7558dc9ae0d8b225a1296bfdb79d61a6d
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-04-20 16:10:05 +0200

    Realizzato slider carousel

--- js/script.js
+++ js/script.js
@@ -1,9 +1,44 @@
 var app = new Vue({
         el: '#root',
         data: {
+            currentImageIndex: 0,
+            images: [
+                {
+                    source: 'img/campagna.jpg',
+                    alt: 'campagna'
+                },
+                {
+                    source: 'img/cascate.webp',
+                    alt: 'cascate'
+                },
+                {
+                    source: 'img/fiori.jpeg',
+                    alt: 'fiori'
+                },
+                {
+                    source: 'img/montagne.webp',
+                    alt: 'montagne'
+                }
+            ]                       
             
         },
         methods: {
-
+            nextImage() {
+                let nextImageIndex = this.currentImageIndex + 1;
+                if (this.currentImageIndex >= this.images.length - 1) {
+                    this.currentImageIndex = 0;
+                } else {
+                    this.currentImageIndex = nextImageIndex;
+                }
+            },
+            prevImage() {
+                let prevImageIndex = this.currentImageIndex -1;
+                if(this.currentImageIndex <= 0) {
+                    this.currentImageIndex = this.images.length - 1;
+                } else {
+                    this.currentImageIndex = prevImageIndex;
+                }
+            }
         }
 });
+
